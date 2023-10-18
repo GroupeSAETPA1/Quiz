73cce4e414d1c58e723d03232f567295d19ccd2b
François SP <francois-sp@gmx.fr>
2023-10-18 15:16:57 +0200

    Update Modelio

---
+++ Modelio/Digramme de classe/src/Chiffrement.java
@@ -0,0 +1,20 @@
+import com.modeliosoft.modelio.javadesigner.annotations.objid;
+
+@objid ("8e227410-285c-4742-9c90-d330a8739c17")
+public class Chiffrement {
+    @objid ("6ab8d7a4-e1db-4df2-87a7-3cf1ac5d289e")
+    private String cleVigener;
+
+    @objid ("cdfe6d34-1054-4cbb-8256-5e112b6eb332")
+    public String genererCleVigener(final int longueur) {
+        // TODO Auto-generated return
+        return null;
+    }
+
+    @objid ("974eef1b-a8aa-4786-825a-7205832bea5a")
+    public String chiffrementVigenere(final String aChiffre) {
+        // TODO Auto-generated return
+        return null;
+    }
+
+}
