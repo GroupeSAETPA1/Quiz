b772a68946be0a4e326bf954d1da423d2ba97c48
CrabeNeo <neo.becogne@iut-rodez.fr>
2023-10-26 14:31:32 +0200

    Commentaire de controlleur editeur, et ajout de la fonction changervue sur editeur et editeur question

--- wkQuiz/ihm-quizz/src/application/controleurs/ControlleurEditeur.java
+++ wkQuiz/ihm-quizz/src/application/controleurs/ControlleurEditeur.java
@@ -1,5 +1,6 @@
 package application.controleurs;
 
+import application.Quiz;
 import javafx.fxml.FXML;
 
 /**
@@ -15,29 +16,53 @@ import javafx.fxml.FXML;
 
 public class ControlleurEditeur {
 	 
-	
+	/**
+	 * Méthode liée au bouton importer des question, 
+	 * qui devra envoyer sur la page ImporterQuestion.fxml
+	 */
 	@FXML 
 	private void importer() {
 		System.out.println("Importer des question");
+		//Quiz.changerVue("ImporterQuestion.fxml");
 	}
 	
+	/**
+	 *  Méthode liée au bouton Créer , 
+	 * qui devra envoyer sur la page CreationQuestionetCategorie.fxml
+	 */
 	@FXML 
 	private void creer() {
 		System.out.println("Creer une categorie ou une question ");
+		//Quiz.changerVue("CreationQuestionetCategorie.fxml");
 	}
 	
+	/**
+	 *  Méthode liée au bouton Editer une question, 
+	 * qui devra envoyer sur la page EditerQuestion.fxml
+	 */
 	@FXML 
 	private void editerQuestion() {
 		System.out.println("Editer une question ");
+		//Quiz.changerVue("EditerQuestion.fxml");
 	}
 	
+	/**
+	 *  Méthode liée au bouton editer une categorie , 
+	 * qui devra envoyer sur la page EditerCategorie.fxml
+	 */
 	@FXML 
 	private void editerCategorie() {
 		System.out.println("Editer une Catégorie ");
+		//Quiz.changerVue("EditerCategorie.fxml");
 	}
 	
+	/**
+	 * Méthode liée au group Accueil, 
+	 * qui devra envoyer sur la page Accueile.fxml
+	 */
 	@FXML 
 	private void accueil() {
 		System.out.println("Retour a l'accueil");
+		//Quiz.changerVue(" Accueile.fxml");
 	}
 }
