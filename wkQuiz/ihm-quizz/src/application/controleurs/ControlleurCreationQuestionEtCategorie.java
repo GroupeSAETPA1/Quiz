bbe3c4371b75ec847831537259272b9e70d9877d
quentinformatique <quentin161204@gmail.com>
2023-10-26 11:43:41 +0200

    replissage de la combobox

--- wkQuiz/ihm-quizz/src/application/controleurs/ControlleurCreationQuestionEtCategorie.java
+++ wkQuiz/ihm-quizz/src/application/controleurs/ControlleurCreationQuestionEtCategorie.java
@@ -1,26 +1,46 @@
 package application.controleurs;
 
+import java.util.ArrayList;
+
+import application.modele.Categorie;
 import javafx.fxml.FXML;
-import javafx.scene.Group;
+import javafx.scene.control.ComboBox;
 
 public class ControlleurCreationQuestionEtCategorie {
-	
-	@FXML 
+
+	private ArrayList<Categorie> categories;
+
+	@FXML
+	private ComboBox selectCategorie;
+
+	@FXML
 	private void retourAcceuil() {
 		System.out.println("Button retour à l'acceuil");
 	}
-	@FXML 
+	@FXML
 	private void annuler() {
 		System.out.println("Annuler");
 	}
-	
-	@FXML 
+
+	@FXML
 	private void valider() {
 		System.out.println("Valider");
 	}
-	
-	// TODO array pour liste deroulante 
-	
-	
-	
+
+	@FXML
+	public void initialize(){
+		// TODO: changer categorie pour qu'elle contiene les vraie categories
+		// BanqueCategorie.getCategories();
+        categories = new ArrayList<>();
+        categories.add(new Categorie("test 1"));
+        categories.add(new Categorie("test 2"));
+        categories.add(new Categorie("test 2"));
+
+        for (Categorie categorie : categories) {
+        	 selectCategorie.getItems().add(categorie);
+		}
+
+    }
+
+
 }
