3ff44ac23c91d63709463f777889a4300f0e60b1
François SP <francois-sp@gmx.fr>
2023-10-30 11:12:18 +0100

    Ajout entete de  méthodes
    Je pense qu'il en manque encore

--- wkQuiz/ihm-quizz/src/application/modele/ModelePrincipal.java
+++ wkQuiz/ihm-quizz/src/application/modele/ModelePrincipal.java
@@ -12,8 +12,10 @@ import application.exception.InvalidNameException;
 import application.exception.ReponseException;
 
 /** 
- * TODO comment class responsibility (SRP)
- * @author Lucas 
+ * Permet l'interaction entre les classes du modèle et les controlleurs.
+ * C'est une classe Singleton
+ * @author Lucas Descriaud
+ * @author François de Saint Palais
  */
 public class ModelePrincipal {
     
@@ -21,14 +23,17 @@ public class ModelePrincipal {
     private BanqueQuestion banqueQuestion;
     private BanqueCategorie banqueCategorie;
     
+    private Question questionAModifier;
+    
     private ModelePrincipal() {
         //TODO instancier les banques 
-        // TODO lire les fichiers serialisé
+        //TODO lire les fichiers serialisé
         this.banqueQuestion = new BanqueQuestion();
         this.banqueCategorie = new BanqueCategorie();
     }
     
     /**
+     * @return Renvoie l'instance unique de ModelePrincipal
      */
     public static ModelePrincipal  getInstance() {
         if (ModelePrincipal.modele == null) {
@@ -38,8 +43,10 @@ public class ModelePrincipal {
     }
     
     public BanqueQuestion getBanqueQuestion() {
+        //TODO Je pense que l'on peux changer la visibilité de certaine méthode de Question. Pour éviter les effet de bord.s
         return banqueQuestion;
     }
+    
     /**
      * Ajoute une question a la modele.banqueQuestion en appelant le
      * constructeur de question. 
@@ -62,7 +69,7 @@ public class ModelePrincipal {
     int difficulte , String reponseJuste , ArrayList<String> reponseFausses , 
     String feedback) throws InvalidFormatException, InvalidNameException, ReponseException {
         
-        boolean questionAjout = false ;
+        boolean questionEstAjoute = false ;
         boolean categorieExistante = categorieValide(categorie);
         ArrayList<Question> listeQuestion = banqueQuestion.getQuestions();
         
@@ -75,12 +82,21 @@ public class ModelePrincipal {
         // on verifie si la question a creer existe si non on l'ajoute 
           if (!listeQuestion.contains(questionAajouter)) {
               listeQuestion.add(questionAajouter);
-              questionAjout  = true ; 
+              questionEstAjoute  = true ; 
            }
         }
         // true question bien ajouter , false sinon
-        return questionAjout;
+        return questionEstAjoute;
         
+    }
+    
+    /**
+     * Créer une Categorie. Si la création a été un sucés on revoie true sinon false
+     * @param nom Le nom que vous voulait donnée à la Catgorie.
+     * @return true si la création est un sucés false sinon
+     */
+    public boolean creerCategorie(String nom) {
+        return false; //STUB
     }
 
     /** 
@@ -95,5 +111,71 @@ public class ModelePrincipal {
         }
         return true; //stub
     }
+    
+    
+    /**
+     * @return Une liste des categories
+     * @throws InvalidNameException //STUB
+     */
+    public ArrayList<Categorie> getCategories() throws InvalidNameException {
+        ArrayList<Categorie> resultat = new ArrayList<Categorie>(3);
+        resultat.add(new Categorie("Premier bouchon"));
+        resultat.add(new Categorie("Second bouchon"));
+        resultat.add(new Categorie("Troisieme bouchon"));
+        
+        return resultat; //STUB
+    }
+    
+    /**
+     * Supprime de la banque de question la question passé en paramètre.
+     * Si la suppression est un sucés alors on revoie true false sinon
+     * @param questionASuprimer
+     * @return true si la suppression est un sucés false sinon
+     */
+    public boolean supprimerQuestion(Question questionASuprimer) {
+        return false; //STUB
+    }
+    
+    /**
+     * @param categorie
+     * @return revoie true si la categorie contient des question false sinon
+     */
+    public boolean categorieContientQuestion(Categorie categorie) {
+        return false; //STUB
+    }
+    
+    /**
+     * Supprimer de la banque de categorie la categorie passé en paramètre.
+     * Si la categorie a supprimer est la categorie "Général", la suppression est annulé
+     * @param categorieASupprimer
+     * @return true si la suppression est un sucés false sinon
+     */
+    public boolean supprimerCategorie(Categorie categorieASupprimer) {
+        return false; //STUB
+    }
+    
+    /**
+     * Pour garder l'instance de la question que l'on veux modifier, 
+     * on garde l'instance de la question dans cet classe
+     */
+    public void setQuestionAModifier(Question questionAModifier) {
+        this.questionAModifier = questionAModifier;
+    }
+    
+    /** @return valeur de questionAModifier */
+    public Question getQuestionAModifier() {
+        return questionAModifier;
+    }
+
+    /**
+     * Modifie la questionAModifier
+     * @return
+     */
+    public boolean modifierQuestion(String libelle ,String categorie ,
+            int difficulte , String reponseJuste , ArrayList<String> reponseFausses , 
+            String feedback) {
+        return false; //STUB
+    }
+
 }
 
