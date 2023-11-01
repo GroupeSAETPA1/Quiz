501951e2957066731d27024743a79523961805c0
quentinformatique <quentin161204@gmail.com>
2023-11-01 15:27:28 +0100

    ajout de la classe alertBox et de sa classe pour les tester

---
+++ wkQuiz/ihm-quizz/src/application/vue/AlertBox.java
@@ -0,0 +1,63 @@
+package application.vue;
+
+import java.util.Optional;
+
+import javafx.application.Application;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonType;
+import javafx.stage.Stage;
+
+/**
+ * classe contenant les differents types d'alertbox
+ * 
+ * @author Quentin COSTES
+ */
+public abstract class AlertBox extends Application {
+
+	/**
+	 * alert box de confirmation 
+	 * @param message
+	 * @return
+	 */
+    public static boolean showConfirmationBox(String message) {
+        Alert alert = new Alert(AlertType.CONFIRMATION);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.setTitle("Confirmation");
+
+        ButtonType buttonTypeYes = new ButtonType("Valider");
+        ButtonType buttonTypeNo = new ButtonType("Annuler");
+
+        alert.getButtonTypes().setAll(buttonTypeYes, buttonTypeNo);
+
+        Optional<ButtonType> result = alert.showAndWait();
+        return result.isPresent() && result.get() == buttonTypeYes;
+    }
+    
+    /**
+     * alert box pour afficher le succés d'une opération
+     * @param message
+     */
+    public static void showSuccessBox(String message) {
+        Alert alert = new Alert(AlertType.INFORMATION);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.setTitle("Succès");
+
+        alert.showAndWait();
+    }
+
+    /**
+     * alert box pour afficher une erreur
+     * @param message
+     */
+    public static void showErrorBox(String message) {
+        Alert alert = new Alert(AlertType.ERROR);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.setTitle("Erreur");
+
+        alert.showAndWait();
+    }
+}
