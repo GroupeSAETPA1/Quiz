1ed8757307e167a726f3cf279952735661c3baee
quentinformatique <quentin161204@gmail.com>
2023-11-08 16:35:58 +0100

    Les elements perdu

--- wkQuiz/ihm-quizz/src/application/controleurs/ControlleurEditerCategories.java
+++ wkQuiz/ihm-quizz/src/application/controleurs/ControlleurEditerCategories.java
@@ -14,8 +14,8 @@ import application.modele.ModelePrincipal;
 import java.util.ArrayList;
 
 import application.Quiz;
-import application.controleurs.factories.EditerCategorieButtonCellFactory;
-import application.controleurs.factories.SupprimerCategorieButtonCellFactory;
+import application.controleurs.factories.EditerButtonCellFactory;
+import application.controleurs.factories.SupprimerButtonCellFactory;
 import application.controleurs.lignes.LigneCategorie;
 import application.exception.HomonymeException;
 import application.exception.InvalidNameException;
@@ -57,34 +57,36 @@ public class ControlleurEditerCategories {
 	    nomColumn.setCellValueFactory(new PropertyValueFactory<>("nomProperty"));
 	    nomColumn.setCellFactory(tc -> {
 	        TableCell<LigneCategorie, String> cell = new TableCell<>();
-	        cell.setAlignment(Pos.CENTER);
 	        cell.textProperty().bind(cell.itemProperty());
 	        cell.setStyle("-fx-font-size: 30px");
 	        return cell;
 	    });
 
-	    TableColumn<LigneCategorie, Integer> nbColumn = new TableColumn<>("Nombre de questions contenues");
+	    TableColumn<LigneCategorie, Integer> nbColumn = new TableColumn<>("Nombre de questions");
 	    nbColumn.setCellValueFactory(new PropertyValueFactory<>("nbProperty"));
 	    nbColumn.setCellFactory(tc -> {
 	        TableCell<LigneCategorie, Integer> cell = new TableCell<>();
-	        cell.setAlignment(Pos.CENTER);
 		    cell.textProperty().bind(cell.itemProperty().asString());
 		    cell.setStyle("-fx-font-size: 30px");
 	        return cell;
 	    });
 
-	    TableColumn<LigneCategorie, String> modifColumn = new TableColumn<>("Modifier la categorie");
-	    modifColumn.setCellFactory(new EditerCategorieButtonCellFactory());
+	    TableColumn<LigneCategorie, String> modifColumn = new TableColumn<>("Modifier");
+	    modifColumn.setCellFactory(new EditerButtonCellFactory());
 
-	    TableColumn<LigneCategorie, String> supColumn = new TableColumn<>("Supprimer la categorie");
-	    supColumn.setCellFactory(new SupprimerCategorieButtonCellFactory());
+	    TableColumn<LigneCategorie, String> supColumn = new TableColumn<>("Supprimer");
+	    supColumn.setCellFactory(new SupprimerButtonCellFactory());
 
-	    /** style de la table */
+	    /* style initial de la table (avec ajouts via mainStyle.css) */
 	    double tableWidth = 1272;
 	    nomColumn.setPrefWidth(tableWidth * 0.45);  
+	    nomColumn.setResizable(false);
 	    nbColumn.setPrefWidth(tableWidth * 0.25);
+	    nbColumn.setResizable(false);
 	    modifColumn.setPrefWidth(tableWidth * 0.13);
+	    modifColumn.setResizable(false);
 	    supColumn.setPrefWidth(tableWidth * 0.14);
+	    supColumn.setResizable(false);
 
 	    table.getColumns().addAll(nomColumn, nbColumn, modifColumn, supColumn);
 	    
