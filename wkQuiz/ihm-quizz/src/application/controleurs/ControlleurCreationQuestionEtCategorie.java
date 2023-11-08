8b691eab928a2a0c414989e1d3a92d04a4e220cd
François SP <francois-sp@gmx.fr>
2023-11-08 13:32:16 +0100

    Fix Erreur

--- wkQuiz/ihm-quizz/src/application/controleurs/ControlleurCreationQuestionEtCategorie.java
+++ wkQuiz/ihm-quizz/src/application/controleurs/ControlleurCreationQuestionEtCategorie.java
@@ -1,7 +1,6 @@
 package application.controleurs;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 
 import application.Quiz;
 import application.exception.HomonymeException;
@@ -13,7 +12,6 @@ import application.modele.ModelePrincipal;
 import application.vue.AlertBox;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
-import javafx.scene.control.Labeled;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
@@ -35,29 +33,28 @@ import javafx.scene.control.ToggleGroup;
 
 public class ControlleurCreationQuestionEtCategorie {
 
-    
-    private ModelePrincipal modele;
-    
-
 
     private ArrayList<Categorie> categories;
 
-	@FXML private ComboBox<Categorie> selectCategorie;
+    private ModelePrincipal modele;
+    
+    @FXML private ToggleGroup difficulte;
+
+	@FXML private TextArea saisieFeedback;
 	
 	@FXML private TextField saisieLibeleQuestion;
+	@FXML private TextField saisieNomCategorie;
 	@FXML private TextField saisieReponseVrai;
+
 	@FXML private TextField saisiePremiereReponseFausse;
 	@FXML private TextField saisieSecondeReponseFausse;
 	@FXML private TextField saisieTroisiemeReponseFausse;
 	@FXML private TextField saisieQuatriemeReponseFausse;
-	@FXML private TextField saisieNomCategorie;
-	
-	@FXML private TextArea saisieFeedback;
-	
-	@FXML private ToggleGroup difficulte;
-	
-	@FXML TabPane tapPane;
+
+	@FXML private ComboBox<Categorie> selectCategorie;
+
 	@FXML Tab tabCategorie;
+	@FXML TabPane tapPane;
 
 	/**
 	 * Méthodes liée au bouton annuler,
@@ -69,101 +66,28 @@ public class ControlleurCreationQuestionEtCategorie {
 		Quiz.changerVue("Acceuil.fxml");
 	}
 
-	@FXML
-	public void initialize() {
-	    System.out.println("Instanciation de Création");
-        // On récupére l'instance du Modéle
-	    modele = ModelePrincipal.getInstance();
-
-	    miseAJourListeCategorie();
-	    
-	    if (ModelePrincipal.getInstance().isDisplayCategoriePane()) {
-	    	System.out.println("ici");
-	    	tapPane.getSelectionModel().select(tabCategorie);
-	    	ModelePrincipal.getInstance().setDisplayCategoriePane(false);
-	    }
-    }
-
-    /** 
-     * TODO comment method role
-     * @throws InvalidNameException
-     */
-    private void miseAJourListeCategorie() {
-        //Récupération puis ajout des nom de catégorie
-        categories = modele.getCategories();
-        selectCategorie.getItems().addAll(categories);
-    }
-
-
 	/**
-	 * Méthodes liée au bouton Accueil
-	 * qui devra renvoyer vers la page Accueil.fxml
-	 */
-	@FXML
-	private void retourAcceuil() {
-		System.out.println("Bouton retour à l'acceuil");
-		Quiz.changerVue("Editeur.fxml");
-	}
-
-	/**
-     * Méthodes liée au bouton valider,
-     * qui devra enregistrer les champs  dans la banques de question
+     * Créer une catégorie dans le modèle et gère les possibles exceptions qui
+     * peuvent apparaître
+     * @param categorieCreer
+     * @param nom
      */
-	@FXML
-	private void valider() {
-	    //TODO Refactor pour avoir une fonction par action
-		System.out.println("Valider");
-		
-		boolean questionCreer = false;
-		
-		//Récupération de l'indice de la catégorie choisie
-		int indiceCategorieChoisie = 
-		        selectCategorie.getSelectionModel().getSelectedIndex();
-		System.out.println(  "Catégorie choisie : " 
-		                   + (indiceCategorieChoisie >= 0 
-		                      ? categories.get(indiceCategorieChoisie) 
-		                      : "Invalide"));
-		
-		//Récupération du nom de la question
-		String libeleQuestion = saisieLibeleQuestion.getText();
-		System.out.println("Nom de la question : " + libeleQuestion);
-		
-		//Récupération de la difficulté
-		int valeurDifficulte = ModelePrincipal.LABEL_DIFFICULTE_TO_INT.get(
-		        ((RadioButton) difficulte.getSelectedToggle()).getText());
-		System.out.println("Difficulté : " + valeurDifficulte);
-		
-		//Récupération du feedback
-		String feedback = saisieFeedback.getText();
-		System.out.println("Feedback : " + feedback);
-		
-		//Récupération de la réponse vrai
-		String reponseVrai = saisieReponseVrai.getText();
-		System.out.println("Réponse vrai : " + reponseVrai);
-		
-		//Récupération des réponses fausses
-		ArrayList<String> mauvaiseReponses = new ArrayList<String>();
-		if (!saisiePremiereReponseFausse.getText().isBlank()) {
-		    mauvaiseReponses.add(saisiePremiereReponseFausse.getText());
+	private void creerEtGererCategorie(boolean categorieCreer,String nom) {
+		try {
+            categorieCreer = modele.creerCategorie(nom);
+
+        } catch (InvalidNameException e) {
+            AlertBox.showErrorBox("Veuillez saisir une nom de catégorie valide ");
+        } catch (HomonymeException e) {
+            AlertBox.showWarningBox("La categorie saisie existe déjà");
         }
-		if (!saisieSecondeReponseFausse.getText().isBlank()) {
-		    mauvaiseReponses.add(saisieSecondeReponseFausse.getText());
-		}
-		if (!saisieTroisiemeReponseFausse.getText().isBlank()) {
-		    mauvaiseReponses.add(saisieTroisiemeReponseFausse.getText());
-		}
-		if (!saisieQuatriemeReponseFausse.getText().isBlank()) {
-		    mauvaiseReponses.add(saisieQuatriemeReponseFausse.getText());
-		}
-		System.out.println("Mauvaise réponse(s) : " + mauvaiseReponses);
-		
-		
-		creerEtGererQuestion(questionCreer, indiceCategorieChoisie, libeleQuestion, valeurDifficulte, feedback,
-                reponseVrai, mauvaiseReponses);
-	}
+		if (categorieCreer) {
+            AlertBox.showSuccessBox("Categorie créer !");
+        }
+    }
 
-    /** 
-     * Créer une question dans le modèle et gère les possibles exceptions qui 
+    /**
+     * Créer une question dans le modèle et gère les possibles exceptions qui
      * peuvent apparaître
      * @param questionCreer
      * @param indiceCategorieChoisie
@@ -177,10 +101,10 @@ public class ControlleurCreationQuestionEtCategorie {
             int valeurDifficulte, String feedback, String reponseVrai, ArrayList<String> mauvaiseReponses) {
         //Création de la question
 		try {
-            questionCreer = modele.creerQuestion(libeleQuestion, 
-                    indiceCategorieChoisie, valeurDifficulte, reponseVrai, 
+            questionCreer = modele.creerQuestion(libeleQuestion,
+                    indiceCategorieChoisie, valeurDifficulte, reponseVrai,
                     mauvaiseReponses, feedback);
-            
+
         } catch (InvalidFormatException e) {
             AlertBox.showErrorBox("Veuillez saisir au minimum une réponse fausse.");
         } catch (InvalidNameException e) {
@@ -197,16 +121,109 @@ public class ControlleurCreationQuestionEtCategorie {
             AlertBox.showSuccessBox("Question créer !");
         }
     }
-    
+
+
+	@FXML
+	public void initialize() {
+	    System.out.println("Instanciation de Création");
+        // On récupère l'instance du Modèle
+	    modele = ModelePrincipal.getInstance();
+
+	    miseAJourListeCategorie();
+
+	    if (ModelePrincipal.getInstance().isDisplayCategoriePane()) {
+	    	System.out.println("ici");
+	    	tapPane.getSelectionModel().select(tabCategorie);
+	    	ModelePrincipal.getInstance().setDisplayCategoriePane(false);
+	    }
+    }
+
+	/**
+     * TODO comment method role
+     * @throws InvalidNameException
+     */
+    private void miseAJourListeCategorie() {
+        //Récupération puis ajout des nom de catégorie
+        categories = modele.getCategories();
+        selectCategorie.getItems().addAll(categories);
+    }
+
+    /**
+	 * Méthodes liée au bouton Accueil
+	 * qui devra renvoyer vers la page Accueil.fxml
+	 */
+	@FXML
+	private void retourAcceuil() {
+		System.out.println("Bouton retour à l'acceuil");
+		Quiz.changerVue("Editeur.fxml");
+	}
+
+    /**
+     * Méthodes liée au bouton valider,
+     * qui devra enregistrer les champs  dans la banques de question
+     */
+	@FXML
+	private void valider() {
+	    //TODO Refactor pour avoir une fonction par action
+		System.out.println("Valider");
+
+		boolean questionCreer = false;
+
+		//Récupération de l'indice de la catégorie choisie
+		int indiceCategorieChoisie =
+		        selectCategorie.getSelectionModel().getSelectedIndex();
+		System.out.println(  "Catégorie choisie : "
+		                   + (indiceCategorieChoisie >= 0
+		                      ? categories.get(indiceCategorieChoisie)
+		                      : "Invalide"));
+
+		//Récupération du nom de la question
+		String libeleQuestion = saisieLibeleQuestion.getText();
+		System.out.println("Nom de la question : " + libeleQuestion);
+
+		//Récupération de la difficulté
+		int valeurDifficulte = ModelePrincipal.LABEL_DIFFICULTE_TO_INT.get(
+		        ((RadioButton) difficulte.getSelectedToggle()).getText());
+		System.out.println("Difficulté : " + valeurDifficulte);
+
+		//Récupération du feedback
+		String feedback = saisieFeedback.getText();
+		System.out.println("Feedback : " + feedback);
+
+		//Récupération de la réponse vrai
+		String reponseVrai = saisieReponseVrai.getText();
+		System.out.println("Réponse vrai : " + reponseVrai);
+
+		//Récupération des réponses fausses
+		ArrayList<String> mauvaiseReponses = new ArrayList<>();
+		if (!saisiePremiereReponseFausse.getText().isBlank()) {
+		    mauvaiseReponses.add(saisiePremiereReponseFausse.getText());
+        }
+		if (!saisieSecondeReponseFausse.getText().isBlank()) {
+		    mauvaiseReponses.add(saisieSecondeReponseFausse.getText());
+		}
+		if (!saisieTroisiemeReponseFausse.getText().isBlank()) {
+		    mauvaiseReponses.add(saisieTroisiemeReponseFausse.getText());
+		}
+		if (!saisieQuatriemeReponseFausse.getText().isBlank()) {
+		    mauvaiseReponses.add(saisieQuatriemeReponseFausse.getText());
+		}
+		System.out.println("Mauvaise réponse(s) : " + mauvaiseReponses);
+
+
+		creerEtGererQuestion(questionCreer, indiceCategorieChoisie, libeleQuestion, valeurDifficulte, feedback,
+                reponseVrai, mauvaiseReponses);
+	}
+
     /**
      * Méthodes liée au bouton valider,
      * qui devra enregistrer les champs  dans la banques Catégori
      */
 	@FXML
     private void validerCategorie() {
-	    
+
 		System.out.println("Valider");
-		
+
 		boolean categorieCreer = false;
 
         String nom = saisieNomCategorie.getText();
@@ -215,23 +232,17 @@ public class ControlleurCreationQuestionEtCategorie {
 		creerEtGererCategorie( categorieCreer, nom);
     }
 	
-    /** 
-     * Créer une catégorie dans le modèle et gère les possibles exceptions qui 
-     * peuvent apparaître
-     * @param categorieCreer
-     * @param nom
-     */
-	private void creerEtGererCategorie(boolean categorieCreer,String nom) {
-		try {
-            categorieCreer = modele.creerCategorie(nom);
-            
-        } catch (InvalidNameException e) {
-            AlertBox.showErrorBox("Veuillez saisir une nom de catégorie valide ");
-        } catch (HomonymeException e) {
-            AlertBox.showWarningBox("La categorie saisie existe déjà");
-        }
-		if (categorieCreer) {
-            AlertBox.showSuccessBox("Categorie créer !");
-        }
-    }
+	/**
+	 * VIde les champs de la créationQuestion
+	 */
+	private void viderChampsQuestion() {
+	    //TODO
+	}
+
+	/**
+	 * VIde les champs de la créationCategorie
+	 */
+	private void viderChampsCategorie() {
+	    //TODO
+	}
 }
