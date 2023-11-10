9716c218cbccfc5af3ac4bfc43ecbe023611781f
Tom Douaud <tom.douaud@iut-rodez.fr>
2023-11-10 14:05:30 +0100

    Merge Commit de la page résultat pour faire son controleur

---
+++ wkQuiz/ihm-quizz/src/application/controleurs/ControleurResultat.java
@@ -0,0 +1,69 @@
+package application.controleurs;
+
+import application.Quiz;
+import javafx.fxml.FXML;
+
+/**
+ * Controlleur de la page Résultat
+ * Celui-ci instancie des méthodes liées aux boutons de la page 
+ * 
+ * @author Tom DOUAUD
+ * @author Néo BECOGNE
+ * @author Quentin COSTES
+ * @author François DE SAINT PALAIS
+ * @author Lucas DESCRIAUD
+ */
+
+
+public class ControleurResultat {
+	
+	/**
+	 * Méthode liée au bouton jouer 
+	 * qui devra renvoyer vers la page ParametrePartie.fxml 
+	 */
+	@FXML 
+	private void jouer() {
+		System.out.println("Button jouer");
+	}
+	
+	
+	/**
+	 * Methode liée au bouton éditer 
+	 * qui devra envoyer vers la page Editeur.fxml 
+	 */
+	@FXML 
+	private void editer() {
+		System.out.println("Button editer");
+		Quiz.changerVue("Editeur.fxml");
+	}
+	
+	/**
+	 * Méthode liée au bouton en ligne
+	 * qui devra envoyer vers la page ModeEnligne.fxml
+	 */
+	@FXML 
+	private void online() {
+		System.out.println("Button en ligne");
+	}
+	
+	/**
+	 * Méthode liée au groupe quitter,
+	 * qui devra fermer l'application
+	 */
+	@FXML
+	private void quitter() {
+		System.out.println("Quitter");
+		Quiz.quitter();
+	}
+	
+	/**
+	 * Méthode liée au groupe aider,
+	 * qui devra envoyer vers la page Aide.fxml
+	 */
+	@FXML
+	private void aider() {
+		System.out.println("Aider");
+		// TODO : lancer une alertBox
+	}
+
+}
