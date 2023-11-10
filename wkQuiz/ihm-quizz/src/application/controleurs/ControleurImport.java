f0cd612f67adcd9648549e4766ebcacdcdbdc9b4
François SP <francois-sp@gmx.fr>
2023-11-10 15:31:27 +0100

    Update controleur ImportQuestion

--- wkQuiz/ihm-quizz/src/application/controleurs/ControleurImport.java
+++ wkQuiz/ihm-quizz/src/application/controleurs/ControleurImport.java
@@ -5,8 +5,13 @@
 
 package application.controleurs;
 
+import java.io.File;
+
+import application.Quiz;
+import application.vue.AlertBox;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
 
 /** 
  * Controlleur de Import
@@ -17,10 +22,23 @@ import javafx.scene.control.TextField;
 public class ControleurImport {
     
     @FXML private TextField saisieCheminFichier;
+    
+    private File fichierCSVChoisie;
 
     @FXML
     private void parcourirExplorer () {
         System.out.println("Parcourir Explorer");
+        FileChooser fichier = new FileChooser();
+        fichier.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("CSV", "*.csv"));
+        fichier.setTitle("Selectionner le fichier contenant les nouvelles questions");
+        
+        fichierCSVChoisie = fichier.showOpenDialog(null);
+        
+        if (fichierCSVChoisie != null) {
+            saisieCheminFichier.setText(fichierCSVChoisie.getAbsolutePath());
+            System.out.println(fichierCSVChoisie.getAbsolutePath()); 
+        }
+        
     }
     
     @FXML
@@ -31,9 +49,44 @@ public class ControleurImport {
     @FXML
     private void retour() {
         System.out.println("Retour");
+        Quiz.changerVue("Editeur.fxml");
     }
+    
     @FXML
     private void valider() {
         System.out.println("Valider");
+        String cheminFichierCSV = saisieCheminFichier.getText();
+        
+        File fichier = new File(cheminFichierCSV);
+        
+        if (!fichier.exists()) {
+            AlertBox.showErrorBox(cheminFichierCSV + ", n'existe pas.");
+        } else if (!cheminFichierCSV.substring(cheminFichierCSV.lastIndexOf(".") + 1).equals("csv")) {
+            AlertBox.showErrorBox(
+                  cheminFichierCSV.substring(cheminFichierCSV.lastIndexOf("\\") + 1)
+                + ", n'est pas un fichier CSV");     
+        } else if (formatCSVValide(fichier)) {
+            
+        }
+    }
+
+    /** 
+     * Un fichier CSV valide contient :
+     * <ul>
+     *     <li>Colonne 1 la catégorie</li>
+     *     <li>Colonne 2 la difficulté</li>
+     *     <li>Colonne 3 le libellé</li>
+     *     <li>Colonne 4 la réponse juste</li>
+     *     <li>Colonne 5 la première réponse fausse</li>
+     *     <li>Colonne 6 la deuxième réponse fausse</li>
+     *     <li>Colonne 7 la troisième réponse fausse</li>
+     *     <li>Colonne 7 la quatrième réponse fausse</li>
+     * </ul>
+     * @param fichierAVerifier Le document à vérifier
+     * @return true si le fichier est valide false sinon
+     */
+    private boolean formatCSVValide(File fichierAVerifier) {
+        // TODO Auto-generated method stub
+        return true; //STUB
     }
 }
