31f9c55d48a8a2696542fd69ee14f8604b50dc5b
Tom Douaud <tom.douaud@iut-rodez.fr>
2023-11-14 08:42:24 +0100

    Bug #91 fixed

--- wkQuiz/ihm-quizz/src/application/controleurs/ControlleurEditeur.java
+++ wkQuiz/ihm-quizz/src/application/controleurs/ControlleurEditeur.java
@@ -53,7 +53,7 @@ public class ControlleurEditeur {
 	@FXML 
 	private void editerCategorie() {
 		System.out.println("Editer une Catégorie ");
-		Quiz.changerVue("EditerCategories.fxml");
+		Quiz.chargerEtChangerVue("EditerCategories.fxml");
 	}
 	
 	/**
