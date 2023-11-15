0f75dcaa77ee3914f755d27ee7c5955caa6a2282
quentinformatique <quentin161204@gmail.com>
2023-11-15 17:33:47 +0100

    affichage du contenue de la question

--- wkQuiz/ihm-quizz/src/application/Quiz.java
+++ wkQuiz/ihm-quizz/src/application/Quiz.java
@@ -133,7 +133,7 @@ public class Quiz extends Application {
 		 
 		primaryStage.setTitle("Quizéo - Accueil");
 		fenetrePrincipale = primaryStage;
-		primaryStage.setScene(scenes.get("ParametrePartie.fxml"));
+		primaryStage.setScene(scenes.get("RepondreQuestion.fxml"));
 		fenetrePrincipale.setResizable(false);
 		primaryStage.show();
 
--- wkQuiz/ihm-quizz/src/application/controleurs/ControlleurRepondreQuestion.java
+++ wkQuiz/ihm-quizz/src/application/controleurs/ControlleurRepondreQuestion.java
@@ -1,5 +1,62 @@
 package application.controleurs;
 
-public class ControlleurRepondreQuestion {
+import application.modele.ModelePrincipal;
+import application.modele.Question;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 
+public class ControlleurRepondreQuestion {	
+	
+	@FXML
+	private Label categorie;
+	
+	@FXML
+	private Label intitule;
+	
+	@FXML
+	private Label difficulte;
+	
+	@FXML
+	private Label numero;
+	
+	@FXML 
+	public void initialize() {
+		afficherQuestion(ModelePrincipal.getInstance().getBanqueQuestion().getQuestions().get(2));
+	}
+
+	private void afficherQuestion(Question question) {
+		
+		categorie.setText(question.getCategorie());
+		
+		String libelle = question.getLibelle();
+		String libelleFormater = "";
+		
+		
+		if (libelle.length() > 179) {
+			intitule.setStyle("-fx-font-size: 22px;");
+			libelleFormater = formaterLIbelle(question.getLibelle(), 80);
+		} else {
+			libelleFormater = formaterLIbelle(question.getLibelle(), 60);
+		}
+		
+		intitule.setText(libelleFormater);
+		difficulte.setText("Difficultée : " + question.getDifficulte());
+		numero.setText("Question n°");// ModelePrincipal.getInstance().getPartie().getNumeroQuestion());
+	
+
+		}
+	
+	private String formaterLIbelle(String chaine, int a) {
+		String libelleFormater = "";
+
+    	for (int i = 0; i <= chaine.length() - 1; i ++) {
+    		libelleFormater += chaine.charAt(i);
+    		if (i % a == 0 && i != 0) {
+    			System.out.println(i);
+    			libelleFormater += "\n";
+    		}
+    	}
+    	
+    	return libelleFormater;   	
+	}
 }
