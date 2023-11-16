61a959b773b0a2cfdd2bdb1f8954e0c813a93074
quentinformatique <quentin161204@gmail.com>
2023-11-16 10:14:06 +0100

    ajout de l'affichage des réponses a la question + agrandissemt fentre  + hashmap difficulté dans modele

--- wkQuiz/ihm-quizz/src/application/controleurs/ControlleurRepondreQuestion.java
+++ wkQuiz/ihm-quizz/src/application/controleurs/ControlleurRepondreQuestion.java
@@ -34,79 +34,115 @@ public class ControlleurRepondreQuestion {
     
     @FXML
     private ToggleGroup reponses ;
-	
-	@FXML 
-	public void initialize() {
-		afficherQuestion(ModelePrincipal.getInstance().getBanqueQuestion().getQuestions().get(10));
-		afficherChoixPossible(ModelePrincipal.getInstance().getBanqueQuestion().getQuestions().get(10));
-		// if déja répondu, on affiche son choix;
-	}
-
-	private void afficherChoixPossible(Question question) {
-		ArrayList<String> reponsePossibles = new ArrayList<>();
-		
-		reponsePossibles.add(question.getReponseJuste());
-		reponsePossibles.addAll(question.getMauvaisesReponses());
-		int nbQuestion = reponsePossibles.size();
-		
-		Collections.shuffle(reponsePossibles);
-		
-		for ( int i = 0 ; i <= reponsePossibles.size() ; 
-		
-	}
-
-	private void afficherQuestion(Question question) {
-		
-		categorie.setText(question.getCategorie());
-		
-		String libelle = question.getLibelle();
-		String libelleFormater = "";
-		
-		
-		if (libelle.length() > 179) {
-			intitule.setStyle("-fx-font-size: 22px;");
-			libelleFormater = formaterLIbelle(question.getLibelle(), 80);
-		} else {
-			libelleFormater = formaterLIbelle(question.getLibelle(), 60);
-		}
-		
-		intitule.setText(libelleFormater);
-		difficulte.setText("Difficultée : " + question.getDifficulte());
-		numero.setText("Question n°");// ModelePrincipal.getInstance().getPartie().getNumeroQuestion());
-	
-
-		}
-	
-	private String formaterLIbelle(String chaine, int a) {
-		String libelleFormater = "";
-
-    	for (int i = 0; i <= chaine.length() - 1; i ++) {
-    		libelleFormater += chaine.charAt(i);
-    		if (i % a == 0 && i != 0) {
-    			System.out.println(i);
-    			libelleFormater += "\n";
-    		}
-    	}
-    	
-    	return libelleFormater;   	
-	}
-
-    
     
     @FXML
-    private void validerReponse() {
-        boolean reponseAlertBox = true ; 
-        Partie partie = ModelePrincipal.getInstance().getPartie();
-        String reponseChoisie; 
-        if (reponses.getSelectedToggle() == null) {
-           reponseAlertBox =  AlertBox.showConfirmationBox("Vous n'avez choisis "
-                   + "aucunes reponses.\nPar défaut cette réponse sera compté "
-                   + "comme fausse");
-        }
-        if (reponseAlertBox) {
-        	reponseChoisie = ((RadioButton) reponses.getSelectedToggle()).getText();
-            partie.setReponseDonnee(partie.getActuelle(), reponseChoisie);
-            Quiz.chargerEtChangerVue("RepondreQuestion.fxml");
-        }
-    }
+    private RadioButton choix1;
+	
+    @FXML
+    private RadioButton choix2;
+    
+    @FXML
+    private RadioButton choix3;
+    
+    @FXML
+    private RadioButton choix4;
+    
+    @FXML
+    private RadioButton choix5;
+   
+    
+   	@FXML 
+   	public void initialize() {
+   		afficherQuestion(ModelePrincipal.getInstance().getBanqueQuestion().getQuestions().get(2));
+   		afficherChoixPossible(ModelePrincipal.getInstance().getBanqueQuestion().getQuestions().get(2));
+   		// if déja répondu, on affiche son choix;
+   	}
+
+   	private void afficherChoixPossible(Question question) {
+   		ArrayList<String> reponsePossibles = new ArrayList<>();
+   		
+   		reponsePossibles.add(question.getReponseJuste());
+   		reponsePossibles.addAll(question.getMauvaisesReponses());
+   		int nbQuestion = reponsePossibles.size();
+   		
+   		Collections.shuffle(reponsePossibles);
+   		
+   		String reponsesAAfficher = "";
+   		
+   		for ( int i = 0 ; i < nbQuestion ; i ++ ) {
+   			if (reponsePossibles.get(i).length() > 125) {
+   				reponsesAAfficher = formaterLibelle(reponsePossibles.get(i), 125);
+   			} else {
+   				reponsesAAfficher = reponsePossibles.get(i);
+   			}
+   			System.out.println(i);
+   			
+   			
+   			if (i == 0) {
+   				choix1.setText(reponsesAAfficher);
+   			} else if (i == 1) {
+   				choix2.setText(reponsesAAfficher);
+   			} else if (i == 2) {
+   				choix3.setText(reponsesAAfficher);
+   			} else if (i == 3) {
+   				choix4.setText(reponsesAAfficher);
+   			} else if (i == 4) {
+   				choix5.setText(reponsesAAfficher);
+   			} 
+   		}
+   		
+   	}
+
+   	private void afficherQuestion(Question question) {
+   		
+   		categorie.setText("Catégorie : " + question.getCategorie());
+   		
+   		String libelle = question.getLibelle();
+   		String libelleFormater = "";
+   		
+   		
+   		if (libelle.length() > 125) {
+   			intitule.setStyle("-fx-font-size: 22px;");
+   		}
+   		libelleFormater = formaterLibelle(question.getLibelle(), 125);
+   		
+   		intitule.setText(libelleFormater);
+   		ModelePrincipal.getInstance();
+		difficulte.setText("Difficulté : " + ModelePrincipal.INT_DIFFICULTE_TO_LABEL.get(question.getDifficulte()));
+   		numero.setText("Question n°");// ModelePrincipal.getInstance().getPartie().getNumeroQuestion());
+   	
+
+   		}
+   	
+   	private String formaterLibelle(String chaine, int a) {
+   		String libelleFormater = "";
+
+       	for (int i = 0; i <= chaine.length() - 1; i ++) {
+       		libelleFormater += chaine.charAt(i);
+       		if (i % a == 0 && i != 0) {
+       			libelleFormater += "\n";
+       		}
+       	}
+       	
+       	return libelleFormater;   	
+   	}
+
+       
+       
+       @FXML
+       private void validerReponse() {
+           boolean reponseAlertBox = true ; 
+           Partie partie = ModelePrincipal.getInstance().getPartie();
+           String reponseChoisie; 
+           if (reponses.getSelectedToggle() == null) {
+              reponseAlertBox =  AlertBox.showConfirmationBox("Vous n'avez choisis "
+                      + "aucunes reponses.\nPar défaut cette réponse sera compté "
+                      + "comme fausse");
+           }
+           if (reponseAlertBox) {
+           	reponseChoisie = ((RadioButton) reponses.getSelectedToggle()).getText();
+               // partie.setReponseDonnee(partie.getActuelle(), reponseChoisie);
+               Quiz.chargerEtChangerVue("RepondreQuestion.fxml");
+           }
+       }
 }
