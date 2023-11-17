40cd66cfb20bcde1aeb9093d54ba86f6c0b817ab
Tom Douaud <tom.douaud@iut-rodez.fr>
2023-11-17 12:01:30 +0100

    Nettoyage et ajout commentaires sur TestBanqueCategories et TestBanqueQuestion

--- wkQuiz/ihm-quizz/src/test/modele/TestBanqueCategorie.java
+++ wkQuiz/ihm-quizz/src/test/modele/TestBanqueCategorie.java
@@ -12,155 +12,148 @@ import application.exception.InvalidNameException;
 import application.modele.BanqueCategorie;
 import application.modele.Categorie;
 
-/** 
- * Classe de test unitaire de la classe {@link application.modele.BanqueCategorie}
+/**
+ * Classe de test unitaire de la classe
+ * {@link application.modele.BanqueCategorie}
+ * 
  * @author Costes Quentin
  * @author Tom Douaud
  */
 class TestBanqueCategorie {
-	
+
 	private ArrayList<Categorie> ensembleCategories;
-	
+
 	private BanqueCategorie banqueCategorie;
 
 	private ArrayList<Categorie> ensembleCategorieLibelleNom;
-	
+
 	/**
-	 * Avant chaque test, initialise la banque de catégorie 
-	 * et les catégories a ajouter dans la banque pour les tests suivant
+	 * Avant chaque test, initialise la banque de catégorie et les catégories a
+	 * ajouter dans la banque pour les tests suivant
 	 * {@link application.modele.BanqueCategorie}
-	 * @throws InvalidNameException 
+	 * 
+	 * @throws InvalidNameException
 	 */
 	@BeforeEach
 	void genererBanque() throws InvalidNameException {
 		banqueCategorie = new BanqueCategorie();
 		ensembleCategories = new ArrayList<Categorie>();
 		ensembleCategorieLibelleNom = new ArrayList<Categorie>();
-		
+
 		ensembleCategories.add(new Categorie("premiere"));
 		ensembleCategories.add(new Categorie("seconde"));
 		ensembleCategories.add(new Categorie("troisieme"));
 		ensembleCategories.add(new Categorie("quatrieme"));
 		ensembleCategories.add(new Categorie("cinquieme"));
-		
+
 	}
-	
+
 	/**
-	 * Test du constructeur de la classe 
+	 * Test du constructeur de la classe
 	 * {@link application.modele.BanqueCategorie#BanqueCategorie()}
 	 */
 	@Test
 	void testBanqueCategorie() {
-		assertDoesNotThrow(()->new BanqueCategorie());
+		assertDoesNotThrow(() -> new BanqueCategorie());
 	}
 
 	/**
-	 * Test de la méthode ajouter et vérifie que les catégories 
-	 * ne sont pas ajoutées si elles ont le même nom (insensible à la casse) 
+	 * Test de la méthode ajouter et vérifie que les catégories ne sont pas ajoutées
+	 * si elles ont le même nom (insensible à la casse)
 	 * {@link application.modele.BanqueCategorie#ajouter(Categorie)}
 	 */
 	@Test
 	void testAjouter() {
-		assertDoesNotThrow(()->banqueCategorie.ajouter(ensembleCategories.get(0)));
-        //Question déjà présente
-        assertThrows(HomonymeException.class, 
-                ()->banqueCategorie.ajouter(ensembleCategories.get(0)));
-        assertThrows(HomonymeException.class, 
-                ()->banqueCategorie.ajouter(new Categorie("PreMIerE")));
+		assertDoesNotThrow(() -> banqueCategorie.ajouter(ensembleCategories.get(0)));
+		// Question déjà présente
+		assertThrows(HomonymeException.class, () -> banqueCategorie.ajouter(ensembleCategories.get(0)));
+		assertThrows(HomonymeException.class, () -> banqueCategorie.ajouter(new Categorie("PreMIerE")));
 	}
 
 	/**
-	 * Test de la méthode getCategories et vérifie que 
-	 * la liste initialisé est vide
+	 * Test de la méthode getCategories et vérifie que la liste initialisé est vide
 	 * {@link application.modele.BanqueCategorie#getCategories()}
-	 * @throws HomonymeException si jamais une catégorie est déjà présente
-	 * @throws InvalidNameException 
+	 * 
+	 * @throws HomonymeException    si jamais une catégorie est déjà présente
+	 * @throws InvalidNameException
 	 */
 	@Test
 	void testGetCategories() throws HomonymeException, InvalidNameException {
-        for (Categorie categorie : ensembleCategories) {
-        	banqueCategorie.ajouter(categorie);
-        }
-        ensembleCategories.add(0,new Categorie("General"));
-        assertIterableEquals(ensembleCategories, banqueCategorie.getCategories());
+		for (Categorie categorie : ensembleCategories) {
+			banqueCategorie.ajouter(categorie);
+		}
+		ensembleCategories.add(0, new Categorie("General"));
+		assertIterableEquals(ensembleCategories, banqueCategorie.getCategories());
 	}
 
 	/**
-	 * Test de la méthode getCategorie en vérifiant que les index invalides
-	 * lèvent bien une exception et que les index valide renvoie bien la catégorie
+	 * Test de la méthode getCategorie en vérifiant que les index invalides lèvent
+	 * bien une exception et que les index valide renvoie bien la catégorie
 	 * {@link application.modele.BanqueCategorie#getCategorie(int)}
+	 * 
 	 * @throws HomonymeException si jamais une catégorie est déjà présente
 	 */
 	@Test
 	void testGetCategorie() throws HomonymeException {
-        assertThrows(IndexOutOfBoundsException.class, ()->banqueCategorie.getCategorie(-1));
-        
-        banqueCategorie.ajouter(ensembleCategories.get(0));
-        assertThrows(IndexOutOfBoundsException.class, ()->banqueCategorie.getCategorie(ensembleCategories.size()));
-        assertEquals(ensembleCategories.get(0), banqueCategorie.getCategorie(1));
+		assertThrows(IndexOutOfBoundsException.class, () -> banqueCategorie.getCategorie(-1));
+
+		banqueCategorie.ajouter(ensembleCategories.get(0));
+		assertThrows(IndexOutOfBoundsException.class, () -> banqueCategorie.getCategorie(ensembleCategories.size()));
+		assertEquals(ensembleCategories.get(0), banqueCategorie.getCategorie(1));
 	}
 
 	/**
-	 * Test de la méthode getCategoriesLibelle et vérifie qu'on puisse
-	 * bien accéder a une catégorie par son nom (insensible à la casse)
-	 * @throws HomonymeException si jamais une catégorie est déjà présente
+	 * Test de la méthode getCategoriesLibelle et vérifie qu'on puisse bien accéder
+	 * a une catégorie par son nom (insensible à la casse)
+	 * 
+	 * @throws HomonymeException    si jamais une catégorie est déjà présente
 	 * @throws InvalidNameException si jamais le nom de la catégorie est invalide
 	 */
 	@Test
 	void testGetCategoriesLibelle() throws HomonymeException, InvalidNameException {
 		ensembleCategorieLibelleNom.add(new Categorie("premiere"));
 		banqueCategorie.ajouter(ensembleCategorieLibelleNom.get(0));
-		
-		assertIterableEquals(ensembleCategorieLibelleNom, 
-				banqueCategorie.getCategoriesLibelle("premiere"));
-        assertIterableEquals(ensembleCategorieLibelleNom, 
-        					 banqueCategorie.getCategoriesLibelle("preMiERe"));
-        
-        
-        ensembleCategorieLibelleNom.add(new Categorie("premiere categorie"));
-        banqueCategorie.ajouter(ensembleCategorieLibelleNom.get(1));
-		assertIterableEquals(ensembleCategorieLibelleNom, 
-				banqueCategorie.getCategoriesLibelle("premiere"));
-        assertIterableEquals(ensembleCategorieLibelleNom, 
-        					 banqueCategorie.getCategoriesLibelle("preMiERe"));
+
+		assertIterableEquals(ensembleCategorieLibelleNom, banqueCategorie.getCategoriesLibelle("premiere"));
+		assertIterableEquals(ensembleCategorieLibelleNom, banqueCategorie.getCategoriesLibelle("preMiERe"));
+
+		ensembleCategorieLibelleNom.add(new Categorie("premiere categorie"));
+		banqueCategorie.ajouter(ensembleCategorieLibelleNom.get(1));
+		assertIterableEquals(ensembleCategorieLibelleNom, banqueCategorie.getCategoriesLibelle("premiere"));
+		assertIterableEquals(ensembleCategorieLibelleNom, banqueCategorie.getCategoriesLibelle("preMiERe"));
 	}
 
 	/**
-	 * Test de la méthode getExactCategoriesLibelle et vérifie qu'on puisse
-	 * bien accéder a une catégorie par son nom exact (insensible à la casse)
-	 * @throws HomonymeException si jamais une catégorie est déjà présente
+	 * Test de la méthode getExactCategoriesLibelle et vérifie qu'on puisse bien
+	 * accéder a une catégorie par son nom exact (insensible à la casse)
+	 * 
+	 * @throws HomonymeException    si jamais une catégorie est déjà présente
 	 * @throws InvalidNameException si jamais le nom de la catégorie est invalide
 	 */
 	@Test
-	void testGetExactCategoriesLibelle() 
-	throws HomonymeException, InvalidNameException {
+	void testGetExactCategoriesLibelle() throws HomonymeException, InvalidNameException {
 		// TODO, modifier car la méthode a changer
 		ensembleCategorieLibelleNom.add(new Categorie("premiere"));
 		banqueCategorie.ajouter(ensembleCategorieLibelleNom.get(0));
-		
-		assertTrue(ensembleCategorieLibelleNom.contains( 
-				banqueCategorie.getCategorieLibelleExact("premiere")));
-        assertFalse(ensembleCategorieLibelleNom.contains(
-        					banqueCategorie.getCategorieLibelleExact("preMiERe")));
-        
-        
-        ensembleCategorieLibelleNom.add(new Categorie("premiere categorie"));
-        banqueCategorie.ajouter(ensembleCategorieLibelleNom.get(1));
-        
-        ArrayList<Categorie> listeUneCategorie = new ArrayList<Categorie>();
-        listeUneCategorie.add(new Categorie("premiere categorie"));
 
+		assertTrue(ensembleCategorieLibelleNom.contains(banqueCategorie.getCategorieLibelleExact("premiere")));
+		assertFalse(ensembleCategorieLibelleNom.contains(banqueCategorie.getCategorieLibelleExact("preMiERe")));
+
+		ensembleCategorieLibelleNom.add(new Categorie("premiere categorie"));
+		banqueCategorie.ajouter(ensembleCategorieLibelleNom.get(1));
+
+		ArrayList<Categorie> listeUneCategorie = new ArrayList<Categorie>();
+		listeUneCategorie.add(new Categorie("premiere categorie"));
 
 		assertEquals(banqueCategorie.getCategorieLibelleExact("categorie"), null);
-		assertEquals(banqueCategorie.getCategorieLibelleExact("premiere categorie"), 
-		        listeUneCategorie.get(0));
+		assertEquals(banqueCategorie.getCategorieLibelleExact("premiere categorie"), listeUneCategorie.get(0));
 	}
 
 	/**
-	 * Test de la méthode toString et vérifie qu'une chaine de caractère est
-	 * renvoyé
+	 * Test de la méthode toString et vérifie qu'une chaine de caractère est renvoyé
 	 * {@link application.modele.BanqueCategorie#toString()}
-	 * @throws HomonymeException 
+	 * 
+	 * @throws HomonymeException
 	 */
 	@Test
 	void testToString() throws HomonymeException {
--- wkQuiz/ihm-quizz/src/test/modele/TestBanqueQuestion.java
+++ wkQuiz/ihm-quizz/src/test/modele/TestBanqueQuestion.java
@@ -21,178 +21,176 @@ import application.modele.BanqueQuestion;
 import application.modele.Categorie;
 import application.modele.Question;
 
-/** 
- * Classe de test unitaire de la classe {@link application.modele.BanqueQuestion}
+/**
+ * Classe de test unitaire de la classe
+ * {@link application.modele.BanqueQuestion}
+ * 
  * @author Francois de Saint Palais
  * @author Tom Douaud
  */
 class TestBanqueQuestion {
-    
-    private ArrayList<Question> ensembleQuestion;
-    private ArrayList<Question> ensembleQuestionCategorieNom;
-    private ArrayList<Question> ensembleQuestionDifficulteFacile;
-    private ArrayList<Question> ensembleQuestionLibelleNom;
-    private BanqueQuestion banqueQuestion;
-    
-    /**
-     * Génère une question valide pour les tests
-     * @throws ReponseException 
-     * @throws InvalidNameException 
-     * @throws InvalidFormatException 
-     * @throws DifficulteException 
-     */
-    @BeforeEach
-    void genererQuestionValide() throws InvalidFormatException, InvalidNameException, ReponseException, DifficulteException {
-        ArrayList<String> reponsesFausses = new ArrayList<String>();
-        reponsesFausses.add("Faux");
-        Question question1 = new Question("Libelle", new Categorie("Nom"), 1,
-                "Vrai", reponsesFausses,"");
-        
-        banqueQuestion = new BanqueQuestion();
-        ensembleQuestion = new ArrayList<Question>();
-        ensembleQuestionCategorieNom = new ArrayList<Question>();
-        ensembleQuestionDifficulteFacile = new ArrayList<Question>();
-        ensembleQuestionLibelleNom = new ArrayList<Question>();
-        
-        ensembleQuestion.add(question1);
-        ensembleQuestionCategorieNom.add(question1);
-        ensembleQuestionDifficulteFacile.add(question1);
-        ensembleQuestionLibelleNom.add(question1);
-        
-    }
 
+	private ArrayList<Question> ensembleQuestion;
+	private ArrayList<Question> ensembleQuestionCategorieNom;
+	private ArrayList<Question> ensembleQuestionDifficulteFacile;
+	private ArrayList<Question> ensembleQuestionLibelleNom;
+	private BanqueQuestion banqueQuestion;
 
-    /**
-     * Méthode de test pour ajouter
-     * {@link application.modele.BanqueQuestion#ajouter(application.modele.Question)}.
-     */
-    @Test
-    void testAjouter() {
-        assertDoesNotThrow(()->banqueQuestion.ajouter(ensembleQuestion.get(0)));
-        // Question déjà présente
-        assertThrows(HomonymeException.class, 
-                ()->banqueQuestion.ajouter(ensembleQuestion.get(0)));
-    }
+	/**
+	 * Génère une question valide pour les tests
+	 * 
+	 * @throws ReponseException
+	 * @throws InvalidNameException
+	 * @throws InvalidFormatException
+	 * @throws DifficulteException
+	 */
+	@BeforeEach
+	void genererQuestionValide()
+			throws InvalidFormatException, InvalidNameException, ReponseException, DifficulteException {
+		ArrayList<String> reponsesFausses = new ArrayList<String>();
+		reponsesFausses.add("Faux");
+		Question question1 = new Question("Libelle", new Categorie("Nom"), 1, "Vrai", reponsesFausses, "");
 
-    /**
-     * Méthode de test pour le constructeur
-     * {@link application.modele.BanqueQuestion#BanqueQuestion(java.util.List)}.
-     */
-    @Test
-    void testBanqueQuestion() {
-        assertDoesNotThrow(()->new BanqueQuestion());
-    }
+		banqueQuestion = new BanqueQuestion();
+		ensembleQuestion = new ArrayList<Question>();
+		ensembleQuestionCategorieNom = new ArrayList<Question>();
+		ensembleQuestionDifficulteFacile = new ArrayList<Question>();
+		ensembleQuestionLibelleNom = new ArrayList<Question>();
 
-    /**
-     * Méthode de test pour getQuestion
-     * {@link application.modele.BanqueQuestion#getQuestion(int)}.
-     * @throws IndexOutOfBoundsException 
-     * @throws HomonymeException
-     */
-    @Test
-    void testGetQuestion() throws IndexOutOfBoundsException, HomonymeException {
-        assertThrows(IndexOutOfBoundsException.class, ()->banqueQuestion.getQuestion(-1));
-        
-        banqueQuestion.ajouter(ensembleQuestion.get(0));
-        assertThrows(IndexOutOfBoundsException.class, ()->banqueQuestion.getQuestion(ensembleQuestion.size()));
-        assertEquals(ensembleQuestion.get(0), banqueQuestion.getQuestion(0));
-    }
+		ensembleQuestion.add(question1);
+		ensembleQuestionCategorieNom.add(question1);
+		ensembleQuestionDifficulteFacile.add(question1);
+		ensembleQuestionLibelleNom.add(question1);
 
-    /**
-     * Méthode de test pour getQuestionsNbFausseReponse
-     * {@link application.modele.BanqueQuestion#getQuestionsNbFausseReponse(int)}.
-     * @throws HomonymeException 
-     * @throws InvalidFormatException 
-     */
-    @Test
-    void testGetQuestionNbFausseReponse() throws HomonymeException,  InvalidFormatException {
-        assertThrows(InvalidFormatException.class, ()->banqueQuestion.getQuestionsNbFausseReponse(Integer.MIN_VALUE));
-        assertThrows(InvalidFormatException.class, ()->banqueQuestion.getQuestionsNbFausseReponse(-1));
-        assertThrows(InvalidFormatException.class, ()->banqueQuestion.getQuestionsNbFausseReponse(0));
-        assertThrows(InvalidFormatException.class, ()->banqueQuestion.getQuestionsNbFausseReponse(5));
-        assertThrows(InvalidFormatException.class, ()->banqueQuestion.getQuestionsNbFausseReponse(Integer.MAX_VALUE));
-        
-        banqueQuestion.ajouter(ensembleQuestion.get(0));
-        assertEquals(ensembleQuestion.get(0), 
-                banqueQuestion.getQuestionsNbFausseReponse(1).get(0));
-    }
+	}
 
-    /**
-     * Méthode de test pour getQuestions
-     * {@link application.modele.BanqueQuestion#getQuestions()}.
-     * @throws HomonymeException 
-     */
-    @Test
-    void testGetQuestions() throws HomonymeException {
-        ArrayList<Question> listeVide = new ArrayList<Question>();
-        assertIterableEquals(listeVide, banqueQuestion.getQuestions());
-        for (Question question : ensembleQuestion) {
-            banqueQuestion.ajouter(question);
-        }
-        assertIterableEquals(ensembleQuestion, banqueQuestion.getQuestions());
-    }
+	/**
+	 * Méthode de test pour le constructeur
+	 * {@link application.modele.BanqueQuestion#BanqueQuestion(java.util.List)}.
+	 */
+	@Test
+	void testBanqueQuestion() {
+		assertDoesNotThrow(() -> new BanqueQuestion());
+	}
+	
+	/**
+	 * Méthode de test pour ajouter
+	 * {@link application.modele.BanqueQuestion#ajouter(application.modele.Question)}.
+	 */
+	@Test
+	void testAjouter() {
+		// On ajoute une question valide qui n'est pas déjà présente
+		assertDoesNotThrow(() -> banqueQuestion.ajouter(ensembleQuestion.get(0)));
+		// La question déjà présente, on renvoie l'exception "HomonymeException"
+		assertThrows(HomonymeException.class, () -> banqueQuestion.ajouter(ensembleQuestion.get(0)));
+	}
 
+	/**
+	 * Méthode de test pour getQuestion
+	 * {@link application.modele.BanqueQuestion#getQuestion(int)}.
+	 * 
+	 * @throws IndexOutOfBoundsException
+	 * @throws HomonymeException
+	 */
+	@Test
+	void testGetQuestion() throws IndexOutOfBoundsException, HomonymeException {
+		assertThrows(IndexOutOfBoundsException.class, () -> banqueQuestion.getQuestion(-1));
 
+		banqueQuestion.ajouter(ensembleQuestion.get(0));
+		assertThrows(IndexOutOfBoundsException.class, () -> banqueQuestion.getQuestion(ensembleQuestion.size()));
+		assertEquals(ensembleQuestion.get(0), banqueQuestion.getQuestion(0));
+	}
 
-    /**
-     * Méthode de test pour getQuestionsCategorie
-     * {@link application.modele.BanqueQuestion#getQuestions(application.modele.Categorie)}.
-     * @throws HomonymeException 
-     * @throws InvalidNameException 
-     */
-    @Test
-    void testGetQuestionsCategorie() throws HomonymeException, InvalidNameException {
-        banqueQuestion.ajouter(ensembleQuestionCategorieNom.get(0));
-        assertEquals(ensembleQuestionCategorieNom.get(0), 
-                banqueQuestion.getQuestions(new Categorie("Nom")).get(0));
-    }
+	/**
+	 * Méthode de test pour getQuestionsNbFausseReponse
+	 * {@link application.modele.BanqueQuestion#getQuestionsNbFausseReponse(int)}.
+	 * 
+	 * @throws HomonymeException
+	 * @throws InvalidFormatException
+	 */
+	@Test
+	void testGetQuestionNbFausseReponse() throws HomonymeException, InvalidFormatException {
+		assertThrows(InvalidFormatException.class, () -> banqueQuestion.getQuestionsNbFausseReponse(Integer.MIN_VALUE));
+		assertThrows(InvalidFormatException.class, () -> banqueQuestion.getQuestionsNbFausseReponse(-1));
+		assertThrows(InvalidFormatException.class, () -> banqueQuestion.getQuestionsNbFausseReponse(0));
+		assertThrows(InvalidFormatException.class, () -> banqueQuestion.getQuestionsNbFausseReponse(5));
+		assertThrows(InvalidFormatException.class, () -> banqueQuestion.getQuestionsNbFausseReponse(Integer.MAX_VALUE));
 
-    /**
-     * Méthode de test pour getQuestionsDifficulte
-     * {@link application.modele.BanqueQuestion#getQuestionsDifficulte(int)}.
-     * @throws HomonymeException 
-     * TODO FIXME
-     * @throws DifficulteException 
-     */
-    @Test
-    void testGetQuestionsDifficulte() throws HomonymeException, DifficulteException {
-        assertThrows(DifficulteException.class, ()->banqueQuestion.getQuestionsDifficulte(Integer.MIN_VALUE));
-        assertThrows(DifficulteException.class, ()->banqueQuestion.getQuestionsDifficulte(-1));
-        assertThrows(DifficulteException.class, ()->banqueQuestion.getQuestionsDifficulte(0));
-        assertThrows(DifficulteException.class, ()->banqueQuestion.getQuestionsDifficulte(4));
-        assertThrows(DifficulteException.class, ()->banqueQuestion.getQuestionsDifficulte(Integer.MAX_VALUE));
-        
-        banqueQuestion.ajouter(ensembleQuestionDifficulteFacile.get(0));
-        assertIterableEquals(ensembleQuestionDifficulteFacile, 
-                banqueQuestion.getQuestionsDifficulte(1));
-    }
+		banqueQuestion.ajouter(ensembleQuestion.get(0));
+		assertEquals(ensembleQuestion.get(0), banqueQuestion.getQuestionsNbFausseReponse(1).get(0));
+	}
 
-    /**
-     * Méthode de test pour getQuestionsLibelle
-     * {@link application.modele.BanqueQuestion#getQuestionsLibelle(java.util.List)}.
-     * @throws HomonymeException 
-     */
-    @Test
-    void testGetQuestionsLibelle() throws HomonymeException {
-        banqueQuestion.ajouter(ensembleQuestionLibelleNom.get(0));
+	/**
+	 * Méthode de test pour getQuestions
+	 * {@link application.modele.BanqueQuestion#getQuestions()}.
+	 * 
+	 * @throws HomonymeException
+	 */
+	@Test
+	void testGetQuestions() throws HomonymeException {
+		ArrayList<Question> listeVide = new ArrayList<Question>();
+		assertIterableEquals(listeVide, banqueQuestion.getQuestions());
+		for (Question question : ensembleQuestion) {
+			banqueQuestion.ajouter(question);
+		}
+		assertIterableEquals(ensembleQuestion, banqueQuestion.getQuestions());
+	}
 
-        assertIterableEquals(ensembleQuestionLibelleNom,
-                banqueQuestion.getQuestionsLibelle("Libelle"));
-        assertIterableEquals(ensembleQuestionLibelleNom, 
-                banqueQuestion.getQuestionsLibelle("libelle"));
-        assertIterableEquals(ensembleQuestionLibelleNom, 
-                banqueQuestion.getQuestionsLibelle("LIBELLE"));
-        // On attend toutes les questions
-        assertIterableEquals(ensembleQuestionLibelleNom, 
-                banqueQuestion.getQuestionsLibelle(""));
-    }
+	/**
+	 * Méthode de test pour getQuestionsCategorie
+	 * {@link application.modele.BanqueQuestion#getQuestions(application.modele.Categorie)}.
+	 * 
+	 * @throws HomonymeException
+	 * @throws InvalidNameException
+	 */
+	@Test
+	void testGetQuestionsCategorie() throws HomonymeException, InvalidNameException {
+		banqueQuestion.ajouter(ensembleQuestionCategorieNom.get(0));
+		assertEquals(ensembleQuestionCategorieNom.get(0), banqueQuestion.getQuestions(new Categorie("Nom")).get(0));
+	}
 
-    /**
-     * Méthode de test pour toString de BanqueQuestion
-     * {@link application.modele.BanqueQuestion#toString()}.
-     */
-    @Test
-    void testToString() {
-        assertTrue(banqueQuestion.toString() instanceof String);
-    }
+	/**
+	 * Méthode de test pour getQuestionsDifficulte
+	 * {@link application.modele.BanqueQuestion#getQuestionsDifficulte(int)}.
+	 * 
+	 * @throws HomonymeException   TODO FIXME
+	 * @throws DifficulteException
+	 */
+	@Test
+	void testGetQuestionsDifficulte() throws HomonymeException, DifficulteException {
+		assertThrows(DifficulteException.class, () -> banqueQuestion.getQuestionsDifficulte(Integer.MIN_VALUE));
+		assertThrows(DifficulteException.class, () -> banqueQuestion.getQuestionsDifficulte(-1));
+		assertThrows(DifficulteException.class, () -> banqueQuestion.getQuestionsDifficulte(0));
+		assertThrows(DifficulteException.class, () -> banqueQuestion.getQuestionsDifficulte(4));
+		assertThrows(DifficulteException.class, () -> banqueQuestion.getQuestionsDifficulte(Integer.MAX_VALUE));
+
+		banqueQuestion.ajouter(ensembleQuestionDifficulteFacile.get(0));
+		assertIterableEquals(ensembleQuestionDifficulteFacile, banqueQuestion.getQuestionsDifficulte(1));
+	}
+
+	/**
+	 * Méthode de test pour getQuestionsLibelle
+	 * {@link application.modele.BanqueQuestion#getQuestionsLibelle(java.util.List)}.
+	 * 
+	 * @throws HomonymeException
+	 */
+	@Test
+	void testGetQuestionsLibelle() throws HomonymeException {
+		banqueQuestion.ajouter(ensembleQuestionLibelleNom.get(0));
+
+		assertIterableEquals(ensembleQuestionLibelleNom, banqueQuestion.getQuestionsLibelle("Libelle"));
+		assertIterableEquals(ensembleQuestionLibelleNom, banqueQuestion.getQuestionsLibelle("libelle"));
+		assertIterableEquals(ensembleQuestionLibelleNom, banqueQuestion.getQuestionsLibelle("LIBELLE"));
+		// On attend toutes les questions
+		assertIterableEquals(ensembleQuestionLibelleNom, banqueQuestion.getQuestionsLibelle(""));
+	}
+
+	/**
+	 * Méthode de test pour toString de BanqueQuestion
+	 * {@link application.modele.BanqueQuestion#toString()}.
+	 */
+	@Test
+	void testToString() {
+		assertTrue(banqueQuestion.toString() instanceof String);
+	}
 }
